bd6028c292217c6181e27b31ef34643c2d661aa7
root <root@BlackSUPREME>
2023-12-23 20:18:46 +0100

    v1.8.3

--- _tools.js
+++ _tools.js
@@ -2,18 +2,18 @@ const path = require('path');
 const fs = require('fs');
 
 function findRootWorkspacePath(currentPath) {        
-    const parentPackageJsonPath = path.join(currentPath + '/..', 'package.json');        
-    const parentPackageDir = path.basename(parentPackageJsonPath);
+  const parentPackageJsonPath = path.join(currentPath + '/..', 'package.json');        
+  const parentPackageDir = path.dirname(parentPackageJsonPath);
 
-    if (fs.existsSync(parentPackageJsonPath)) {
-      const packageJson = JSON.parse(fs.readFileSync(parentPackageJsonPath, 'utf-8'));
+  if (fs.existsSync(parentPackageJsonPath)) {
+    const packageJson = JSON.parse(fs.readFileSync(parentPackageJsonPath, 'utf-8'));
 
-      if (packageJson.workspaces) {
-        return findRootWorkspacePath(parentPackageDir);
-      }
+    if (packageJson.workspaces) {
+      return this.findRootWorkspacePath(parentPackageDir);
     }
+  }
 
-    return currentPath;
+  return currentPath;
   }
 
 module.exports = {
